bf59dac1b1b85d829049981d8900ea6f6cdb873f
Timothy Olisaeloka Ngonadi <timothyolisaeloka@gmail.com>
2024-10-31 00:04:05 +0100

    initial commit

---
+++ src/main/java/com/simba/iaas/IaasApplication.java
@@ -0,0 +1,13 @@
+package com.simba.iaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaasApplication.class, args);
+	}
+
+}
